1049c9394c9118982c4b52c4f2fc523211b00902
mariaantony-gnanasekaran <mariaantony.gnanasekaran@syncfusion.com>
2017-03-24 12:17:57 +0530

    Configured aurelia-syncfusion-bridge with Aurelia application in skeleton-typescript-aspnetcore

--- skeleton-typescript-aspnetcore/src/skeleton/src/main.ts
+++ skeleton-typescript-aspnetcore/src/skeleton/src/main.ts
@@ -4,7 +4,8 @@ import {Aurelia} from 'aurelia-framework';
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
+    .developmentLogging()
+    .plugin('aurelia-syncfusion-bridge', (syncfusion) => syncfusion.useAll());
 
   // Uncomment the line below to enable animation.
   // aurelia.use.plugin('aurelia-animator-css');
